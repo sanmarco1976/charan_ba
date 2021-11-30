7a5f82b024e67e56e98580917b94b64edf353c8f
sanmarco1976 <juanmsanmarco06@gmail.com>
2021-11-29 23:38:54 -0300

    archivo nuevo

--- js/main.js
+++ js/main.js
@@ -1,51 +1,78 @@
-//todo esto es para electronica.html
+const card = document.getElementById('principio');
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
+const templateFooter = document.getElementById('template-footer').content;
+const templateCarrito = document.getElementById('template-carrito').content;
+const fragmento = document.createDocumentFragment();
+let carrito = {};
 
-for (const productos of nombreProductosElectronica) {
-    $('#principio').append(
-        `
-        <section id="secCate">
-            <div class="divCate1--divCate${productos.id} item">
-                <img src="${productos.img}" class="divCate1__imgCate">
-                <p class="divCate1__pCate">${productos.nombre}</p>
-                <ul>
-                    <li class="descripcion">$${productos.precio}</li>
-                    <li class="descripcion--mayor">consultar precio por mayor.</li>
-                    <button type="button" class="btn btn-outline-warning add">Agregar Al Carrito</button>
-                </ul>
-            </div>
-        </section>
-        `
-    );
+//estos if hacen que al estar en la ventana de cada categoria APIURL tenga la direecion de los productos de esa categoria
+let APIURL;
+if (window.location.pathname == '/html/electronica.html') {
+    APIURL = '../js/productosElectro.json';
 }
-const APIURL = 'https://jsonplaceholder.typicode.com/users';
+if (window.location.pathname == '/html/iluminacion.html') {
+    APIURL = '../js/productosILU.json';
+}
+if (window.location.pathname == '/html/hogarydeco.html') {
+    APIURL = '../js/productosHYD.json';
+}
+if (window.location.pathname == '/html/seguridad.html') {
+    APIURL = '../js/productosSEG.json';
+}
+if (window.location.pathname == '/html/belleza.html') {
+    APIURL = '../js/productosBelle.json';
+}
+if (window.location.pathname == '/html/accesorios.html') {
+    APIURL = '../js/productosAcc.json';
+}
+if (window.location.pathname == '/html/audio.html') {
+    APIURL = '../js/productosAU.json';
+}
+//tomamos los datos de nuestra URL
 $.get(APIURL, function(respuesta, estado) {
-    console.log(respuesta);
-    console.log(estado);
-    respuesta.forEach((el) => {
-        $('.usuarios').append(`<li class ="usuariosLi "> Nombre: ${el.name} // Email: ${el.email} </li>`);
+    // console.log(respuesta);
+    // console.log(estado);
+    //creamos en todos los html las cards coon los productos
+    respuesta.forEach((productos) => {
+        $('#principio').append(
+            `
+            <section id="secCate">
+                <div class="divCate1 item">
+                    <img src="${productos.img}" class="divCate1__imgCate">
+                    <p class="divCate1__pCate">${productos.nombre}</p>
+                    <ul>
+                        <li class="descripcion">$${productos.precio}</li>
+                        <li class="descripcion--mayor">consultar precio por mayor.</li>
+                        <button type="button" class="btn btn-outline-warning add" data-id ="${productos.id}">Agregar Al Carrito</button>
+                    </ul>
+                </div>
+            </section>
+            `
+        );
     });
 });
-
-let botonCarrito = document.getElementsByClassName("add");
-
-
-//para recorrer todos los botones de 'agregar al carrito', ya que son un array
-for (const boton of botonCarrito) {
-    boton.addEventListener("click", agregarCarrito);
-    const botonComprar = document.querySelector('.comprar');
-    botonComprar.addEventListener('click', comprarClick);
-    //agrega los items al carrito
-    function agregarCarrito(e) {
-        const botonCadaUno = e.target;
-        const divCompleto = botonCadaUno.closest('.item');
-
-        const divTitulo = divCompleto.querySelector('.divCate1__pCate').textContent;
-        const divPrecio = divCompleto.querySelector('.descripcion').textContent;
-        const divImg = divCompleto.querySelector('.divCate1__imgCate').src;
-        divEnCarrito(divTitulo, divPrecio, divImg);
-
-
+// esta funcion revisa el localStorage y si el array carrito tiene algo lo agrega
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('miCarrito')) {
+        carrito = JSON.parse(localStorage.getItem('miCarrito'));
+        añadirAlCarrito();
     }
+});
+card.addEventListener('click', e => {
+    agregarAlCarrito(e);
+});
+items.addEventListener('click', e => {
+    accionBotones(e);
+});
+// agrega los datos que obtenemos en la funcion infoCarrito al carrito
+const agregarAlCarrito = e => {
+    // console.log(e.target)
+    // console.log(e.target.classList.contains('add'))
+    if (e.target.classList.contains('add')) {
+        infoCarrito(e.target.parentElement.parentElement)
+    }
+    e.stopPropagation();
     $(".add").on('click', function() {
         Swal.fire(
             'Listo!',
@@ -53,95 +80,103 @@ for (const boton of botonCarrito) {
             'success'
         )
     });
-
-    // crea la tabla  y agrega al carrito los elementos recibiendo como parametro las constantes de arriba (tirulo y precio)
-    function divEnCarrito(divTitulo, divPrecio, divImg) {
-        //antes de crear la tabla revisamos si ya esta el producto
-        const titulosElementos = tablaJs.getElementsByClassName('tdTitulo');
-        //este for recorre toda la tabla y se fija si esta
-        for (let i = 0; i < titulosElementos.length; i++) {
-            //si esta sumamos el valor del input
-            if (titulosElementos[i].innerText === divTitulo) {
-                let valorInput = titulosElementos[i].parentElement.querySelector('.tdCant-input');
-                valorInput.value++;
-                //el return es para que corte la funcion y no se agregue el producto
-                mostrarPrecioTotal();
-                return;
-            }
-        }
-        //creamos la tabla
-        let tablaBody = document.createElement("tbody");
-
-        //crea la tabla con los elementos
-        let fila = document.createElement("tr");
-        fila.innerHTML = `
-        <td class="tdImg"><img class="tImg" src=${divImg}></td>
-        <td class="tdTitulo">${divTitulo}</td>
-        <td class="tdPrecio">${divPrecio}</td>
-        <td class="tCant"><input class="tdCant-input" type="number" value="1"></td>
-        <td class="tdBorrar"><button type="button" class="btn-close" aria-label="Close"></button></td>`;
-        tablaBody.appendChild(fila);
-        document.getElementById("tablaJs").appendChild(tablaBody);
-        tablaBody.querySelector('.btn-close').addEventListener('click', eliminarElementos);
-        //otra forma de eliminar los productos
-        // $(".btn-close").click((e) => {
-        //     const botonCerrado = e.target;
-        //     botonCerrado.closest('table').remove();
-        //     mostrarPrecioTotal();
-        // });
-        //llamamos a la funcion para mostrar el precio
-        mostrarPrecioTotal();
-        tablaBody.querySelector('.tdCant-input').addEventListener('change', cambiarvalor);
-
-
-    }
-
-    //funcion   para mostrar el precio TOTAL
-    function mostrarPrecioTotal() {
-        //arranca en 0
-        let total = 0;
-        let totalCarrito = document.querySelector('.totalCarrito');
-
-        const tablasCompleta = document.querySelectorAll('tbody');
-        //esto hace que por cada tabla, agarre al precio, modifique el signo y lo sume
-        tablasCompleta.forEach(tablaCompleta => {
-            const precioTablaCompleta = tablaCompleta.querySelector('.tdPrecio');
-            //modifica el $ por un string vacio
-            const precioTabla = Number(precioTablaCompleta.textContent.replace('$', ''));
-            const cantTabla = tablaCompleta.querySelector('.tdCant-input');
-            //pide el valor del input (la cantidad)
-            const valorCantTabla = Number(cantTabla.value);
-            //sume el precio que ya teniamos con el precio del producto por la cantidad
-            total = total + precioTabla * valorCantTabla;
-        });
-        totalCarrito.innerHTML = `$${total.toFixed(2)}`
-    }
-    //eliminar del carrito
-    function eliminarElementos(e) {
-        const botonCerrado = e.target;
-        botonCerrado.closest('tbody').remove();
-        mostrarPrecioTotal();
-    }
-    //esta funcion hace que cambie el valor del input, pero que no sea ni 0 ni menor a 0
-    function cambiarvalor(e) {
-        const input = e.target;
-        if (input.value <= 0) {
-            input.value = 1;
-        }
-        mostrarPrecioTotal();
-    }
-    //esta funcion hace que cuando clickeamos en comprar, se borre todo el carrito y sal
-    function comprarClick() {
-        tablaJs.innerHTML = '';
-        mostrarPrecioTotal();
-        Swal.fire(
-            'Listo!',
-            'Ya Se Registro Su Compra!',
-            'success'
-        );
-
-    }
 }
+
+// creamos una const de productos para llenar el id, nombre, precio, la imagen y la cantidad
+const infoCarrito = objecto => {
+    const producto = {
+        id: objecto.querySelector('.add').dataset.id,
+        nombre: objecto.querySelector('.divCate1__pCate').textContent,
+        precio: Number(objecto.querySelector('.descripcion').textContent.replace('$', '')),
+        img: objecto.querySelector('.divCate1__imgCate').src,
+        cant: 1,
+    }
+
+    //sumamos cantidad si agrega el mismo producto 
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cant = carrito[producto.id].cant + 1;
+    }
+    carrito[producto.id] = {...producto }
+    añadirAlCarrito();
+}
+
+// crea la tabla del carrito y pinta los datos
+const añadirAlCarrito = () => {
+    items.innerHTML = '';
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id;
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre;
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cant;
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id;
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id;
+        templateCarrito.querySelector('span').textContent = producto.cant * producto.precio;
+        const copiar = templateCarrito.cloneNode(true);
+        fragmento.appendChild(copiar);
+    })
+    items.appendChild(fragmento);
+    precioTotal();
+    localStorage.setItem('miCarrito', JSON.stringify(carrito));
+}
+
+//calcula el precio total 
+const precioTotal = () => {
+    footer.innerHTML = '';
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacío </th>
+        `;
+        return
+    }
+
+    //creamos un acumulador y a este le sumamos la cantidad para que nos de la cantidad de productos que selecciona
+    const cantidadTotal = Object.values(carrito).reduce((acumulador, { cant }) => acumulador + cant, 0);
+    //creamos un acumulador y a este le sumamos la cantidad multiplicado al precio  para que nos de el precio total de los productos que selecciona
+    const precioTotal = Object.values(carrito).reduce((acumulador, { cant, precio }) => acumulador + (cant * precio), 0);
+    templateFooter.querySelectorAll('td')[0].textContent = cantidadTotal;
+    templateFooter.querySelector('span').textContent = precioTotal;
+    //creamos una copia 
+    const copiar = templateFooter.cloneNode(true);
+    fragmento.appendChild(copiar);
+    footer.appendChild(fragmento);
+    const vaciarCarrito = document.getElementById('vaciar-carrito');
+    //funcion para vaciar el carrito, si hace click se eliminan todos los datos del array
+    vaciarCarrito.addEventListener('click', () => {
+        carrito = {};
+        añadirAlCarrito();
+    })
+}
+const accionBotones = e => {
+    //si pulsa el boton + se suma uno a la cantidad
+    if (e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.dataset.id];
+        producto.cant = carrito[e.target.dataset.id].cant + 1;
+        carrito[e.target.dataset.id] = {...producto };
+        añadirAlCarrito();
+    }
+    //si pulsa el boton - se resta uno a la cantidad
+    if (e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id];
+        producto.cant = carrito[e.target.dataset.id].cant - 1;
+        if (producto.cant === 0) {
+            delete carrito[e.target.dataset.id];
+        }
+        añadirAlCarrito();
+    }
+    e.stopPropagation();
+}
+
+//al presionar el boton comprar se elimina todo, en un futuro llegaran los datos de la compra
+$('#comprar-carrito').click(function() {
+    items.innerHTML = '';
+    precioTotal();
+    carrito = {};
+    añadirAlCarrito();
+    Swal.fire(
+        'Listo!',
+        'Ya Se Registro Su Compra!',
+        'success'
+    );
+});
 
 //si apretamos en ver carrito se cambia a ocultar carrito
 $("#botonCarritoo").click(function() {
@@ -153,6 +188,7 @@ $("#botonCarritoo").click(function() {
     }
 
 });
+
 //animacion de scroll para 'ir arriba' del footer
 $("#arriba").click(function() {
     $('html').animate({
--- js/productosSEG.js
+++
@@ -1,8 +0,0 @@
-const nombreProductosElectronica = [
-    { id: 1, nombre: "Camara IP Wifi", precio: 4850, img: "../imagen/camaraIPWifi.jpeg" },
-    { id: 2, nombre: "Alarma detector de humo", precio: 400, img: "../imagen/DETECTOR-DE-HUMO.jpg" },
-    { id: 3, nombre: "Lámpara Espía", precio: 5250, img: "../imagen/camlamp.jpg" },
-    { id: 4, nombre: "Alarma sensor con movimiento", precio: 1800, img: "../imagen/alarmaSensor.jpeg" },
-    { id: 5, nombre: "Baliza solar ms-465", precio: 1700, img: "../imagen/baliza.jpg" },
-    { id: 6, nombre: "Timbre Inalámbrico", precio: 920, img: "../imagen/timbre.jpg" },
-]
